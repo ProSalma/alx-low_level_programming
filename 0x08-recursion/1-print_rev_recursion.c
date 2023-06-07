6815b69ffe42960b21265790ec0acabe6efd4aef
ProSalma <as.ibrahimsalma@gmail.com>
2023-06-07 09:34:19 -0700

    Modification in the file1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,6 +11,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	return;
-	_print_rev_recursion(++s); /*Do the string recursion first*/
+	_print_rev_recursion(s + 1); /*Do the string recursion first*/
 	_putchar(*s); /*Prints the string*/
 }
