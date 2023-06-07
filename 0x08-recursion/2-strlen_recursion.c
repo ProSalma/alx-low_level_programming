bd0c3091935b19d14300d824d52080979fc3426c
ProSalma <as.ibrahimsalma@gmail.com>
2023-06-07 10:17:58 -0700

    Recreate Task 2 file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,22 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _strlen_recursion - This function returns  the lenth of a string
+ * _strlen_recursion - This function returns length of string
  * @s: The pointer to string
- * Return: Void
+ * Return: To int
  * @BySalma.as
  */
+
 
 int _strlen_recursion(char *s)
 {
 	int i = 0;
-	if (*s == '\0')
-	{	
-        i += _strlen_recursion(s + 1) + 1;
-	}
-	return (i);
 
+	if (*s)
+	{
+		i = i + 1;
+		i += _strlen_recursion(s + 1);
+	}
+
+	return (i);
 }
