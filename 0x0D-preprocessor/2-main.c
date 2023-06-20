80060950a218935272f9dae761fab0483ddcca03
ProSalma <as.ibrahimsalma@gmail.com>
2023-06-20 10:21:01 -0700

    0x0D-preprocessor tasks Files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Function prints the name of the file it was compiled from
+ * Return: To 0
+ * BySalma.as
+ */
+
+int	main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
