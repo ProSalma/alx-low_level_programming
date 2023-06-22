a948e9d71e04125302059cf66abc59f929ce983b
ProSalma <as.ibrahimsalma@gmail.com>
2023-06-21 18:11:20 -0700

    File 2 modified

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,18 +11,23 @@
   * @BySalma.as
   */
 
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int x = 0;
+	int i = 0;
 
-	if (!array || !cmp || size <= 0)
-		return (-1);
-	while (x < size)
+	if (size > 0)
 	{
-		if (cmp(array[x]))
-			return (x);
-		x++;
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
 	}
+
 	return (-1);
 }
