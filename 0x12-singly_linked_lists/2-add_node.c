492036ebb78aae2221daa442ad14e9253a7f7665
ProSalma <as.ibrahimsalma@gmail.com>
2023-07-02 13:53:32 -0700

    Mandatory tasks files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,25 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /**
- * _strlen - Computes the length of a string.
- * @str: String to compute the length of.
+ * _strlen - Finds the length of a string.
+ * @str: String to find the length of.
  *
- * Return: The length of the string.
+ * Return: Length of the string.
  */
-
-
-unsigned int _strlen(const char *str)
+unsigned int _strlen(char *str)
 {
 	unsigned int length = 0;
 
-	while (str[length] != '\0')
-		length++;
-
+	for (; str[length]; length++)
+		;
 	return (length);
 }
-
 
 /**
  * add_node - Adds a node to the beginning of a linked list.
@@ -29,27 +24,23 @@ unsigned int _strlen(const char *str)
  *
  * Return: Pointer to the new node.
  */
-
-
 list_t *add_node(list_t **head, const char *str)
 {
-	if (!str)
-		return (NULL);
+	list_t *new_node;
 
-	list_t *new_node = malloc(sizeof(list_t));
-	if (!new_node)
+	if (str == NULL)
 		return (NULL);
-
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->str = strdup(str);
-	if (!new_node->str)
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->len = _strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
-
 	return (new_node);
 }
